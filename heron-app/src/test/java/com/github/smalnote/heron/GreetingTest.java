4c2cc9cac635c46cf97a3afd05ea251f0a5b1f8b
smalnote <smalnote@gmail.com>
2019-12-29 11:52:12 +0800

    remove warnings

--- heron-app/src/test/java/com/github/smalnote/heron/GreetingTest.java
+++ heron-app/src/test/java/com/github/smalnote/heron/GreetingTest.java
@@ -1,15 +1,1 @@
-package com.github.smalnote.heron;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-import org.junit.Test;
-
-public class GreetingTest extends HeronAppTests {
-	
-	@Test
-	public void test() throws Exception {
-		/*this.mockMvc.perform(get("/greet").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
-				.andExpect(status().isOk());*/
-	}
-
-}
+package com.github.smalnote.heron;import org.junit.Test;public class GreetingTest extends HeronAppTests {		@Test	public void test() {		/*this.mockMvc.perform(get("/greet").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))				.andExpect(status().isOk());*/	}}
