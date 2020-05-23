8b3d22f1cb866dd98ab45d8eac58051afcfe1f59
smalnote <smalnote@gmail.com>
2020-05-23 10:10:23 +0800

    add custom bean post processor for debug

---
+++ heron-spring/src/main/java/com/github/smalnote/spring/aspect/CustomBeanPostProcessor.java
@@ -0,0 +1,21 @@
+package com.github.smalnote.spring.aspect;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+    public Object postProcessBeforeInitialization(@Nullable Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+    public Object postProcessAfterInitialization(@Nullable Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+    protected void breakPostProcess() {
+        throw new UnsupportedOperationException("just for break ");
+    }
+}
